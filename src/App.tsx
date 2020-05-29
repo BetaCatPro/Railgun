027c8dbc122252569079056b827c4db86ab61b0f
BetaCatPro <zj20162325@163.com>
2020-05-29 20:59:51 +0800

    fixed button component bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
 import React from 'react';
-import Button, { ButtonType, ButtonSize } from './components/Button/button'
-import Menu from './components/Menu/menu'
-import MenuItem from './components/Menu/menuItem'
-import SubMenu from './components/Menu/subMenu'
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <Menu onSelect={(index)=>{console.log(index)}} mode="veetical">
-          <MenuItem>demo 0</MenuItem>
-          <SubMenu title={'dropitem'}>
-            <MenuItem>demo 11</MenuItem>
-            <MenuItem>demo 22</MenuItem>
-          </SubMenu>
-          <MenuItem>demo 1</MenuItem>
-          <MenuItem>demo 2</MenuItem>
-        </Menu>
-      </header>
-        <Button>Hello</Button>
-        <Button btnType={ButtonType.Primary}>ew</Button>
-        <Button size={ButtonSize.Large}>big</Button>
-        <Button btnType={ButtonType.Link} href="www.baidu.com">baidu</Button>
     </div>
   );
 }
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,67 +1,63 @@
-import React from 'react'
+import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react'
 import classNames from 'classnames'
-
-// export enum ButtonSize {
-//     Large = 'lg',
-//     Small = 'sm'
-// }
-//
-// export enum ButtonType {
-//     Primary = 'primary',
-//     Default = 'default',
-//     Danger = 'fanger',
-//     Link = 'link'
-// }
 
 export type ButtonSize = 'lg' | 'sm'
 export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
 
 interface BaseButtonProps {
-    className?: string
-    disabled?: boolean
-    size?: ButtonSize
-    btnType?: ButtonType
-    href?: string
-    children: React.ReactNode
+    className?: string;
+    /**设置 Button 的禁用 */
+    disabled?: boolean;
+    /**设置 Button 的尺寸 */
+    size?: ButtonSize;
+    /**设置 Button 的类型 */
+    btnType?: ButtonType;
+    children: React.ReactNode;
+    href?: string;
 }
-
-// 原生button属性
-type NativaButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>
-type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>
-export type ButtonProps =  Partial<NativaButtonProps & AnchorButtonProps>
-
-const Button: React.FC<ButtonProps> = (props) => {
+type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>
+type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+/**
+ * 页面中最常用的的按钮元素，适合于完成特定的交互
+ * ### 引用方法
+ *
+ * ~~~js
+ * import { Button } from 'railgun'
+ * ~~~
+ */
+export const Button: FC<ButtonProps> = (props) => {
     const {
-        className,
         btnType,
+        className,
         disabled,
         size,
         children,
         href,
         ...restProps
     } = props
-
-    //btn btn-lg
-    const classes = classNames('btn',className, {
+    // btn, btn-lg, btn-primary
+    const classes = classNames('btn', className, {
         [`btn-${btnType}`]: btnType,
         [`btn-${size}`]: size,
-        'disabled': (btnType === ButtonType.Link) && disabled
+        'disabled': (btnType === 'link') && disabled
     })
-
-    if(btnType === ButtonType.Link && href) {
+    if (btnType === 'link' && href ) {
         return (
-            <a className={classes}
-               href={href}
-               {...restProps}
+            <a
+                className={classes}
+                href={href}
+                {...restProps}
             >
                 {children}
             </a>
         )
     } else {
         return (
-            <button className={classes}
-                    disabled={disabled}
-                    {...restProps}
+            <button
+                className={classes}
+                disabled={disabled}
+                {...restProps}
             >
                 {children}
             </button>
@@ -71,7 +67,7 @@ const Button: React.FC<ButtonProps> = (props) => {
 
 Button.defaultProps = {
     disabled: false,
-    btnType: ButtonType.Default
+    btnType: 'default'
 }
 
-export default Button
+export default Button;
