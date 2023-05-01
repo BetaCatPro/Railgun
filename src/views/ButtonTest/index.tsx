27d59593d9c36ca57816f0af75e49242daec126b
BetaCatPro <zj20162325@163.com>
2023-05-01 15:09:25 +0800

    feat: 完善 Button 组件

--- src/views/ButtonTest/index.tsx
+++ src/views/ButtonTest/index.tsx
@@ -6,11 +6,21 @@ export const ButtonTest = () => {
     return (
         <div>
             <Button size="sm">Button</Button>
-            <Button btnType="primary" size="sm">Button</Button>
-            <Button btnType="danger" size="lg">Button</Button>
-            <Button btnType="warning" size="lg" disabled>Button</Button>
-            <Button btnType="link" href="https://bilibili.com">B站</Button>
-            <Button btnType="link" href="https://bilibili.com" disabled>B站</Button>
+            <Button btnType="primary" size="sm">
+                Button
+            </Button>
+            <Button btnType="danger" size="lg">
+                Button
+            </Button>
+            <Button btnType="warning" size="lg" disabled>
+                Button
+            </Button>
+            <Button btnType="link" href="https://bilibili.com">
+                B站
+            </Button>
+            <Button btnType="link" href="https://bilibili.com" disabled>
+                B站
+            </Button>
         </div>
     )
 }
