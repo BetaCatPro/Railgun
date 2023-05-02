dcedc38e2d907c53929af42df5a7394a42b6fc40
BetaCatPro <zj20162325@163.com>
2023-05-02 16:31:05 +0800

    feat: 完成 Input 组件及测试

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,16 @@ const config: StorybookConfig = {
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/preset-create-react-app',
-        '@storybook/addon-interactions'
+        '@storybook/addon-interactions',
+        {
+            name: '@storybook/addon-styling',
+            options: {
+                sass: {
+                    // Require your Sass preprocessor here
+                    implementation: require('sass')
+                }
+            }
+        }
     ],
     framework: {
         name: '@storybook/react-webpack5',
